97f67b032631434f381a85fdee700bdf0fcbbfdd
steve111mv <unlivregratuit@gmail.com>
2020-09-11 21:20:03 +0100

    Implemented a Splash Screen

--- app/src/main/java/cg/stevendende/gadsleaderboard/MainActivity.java
+++ app/src/main/java/cg/stevendende/gadsleaderboard/MainActivity.java
@@ -7,6 +7,7 @@ import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
---
+++ app/src/main/java/cg/stevendende/gadsleaderboard/SplashScreenActivity.java
@@ -0,0 +1,59 @@
+package cg.stevendende.gadsleaderboard;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.ViewCompat;
+
+import cg.stevendende.gadsleaderboard.databinding.ActivitySplashBinding;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    private static final int UI_DELAY = 3000;
+
+    Handler mHandler;
+    Runnable mRunnable;
+
+    private ActivitySplashBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivitySplashBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        //Enable full screen
+        binding.getRoot().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+
+        //annimate logo size
+        ViewCompat.animate(binding.logo).scaleX(1f).setDuration(1000).start();
+        ViewCompat.animate(binding.logo).scaleY(1f).setDuration(1000).start();
+
+        mRunnable = new Runnable() {
+                @Override
+                public void run() {
+
+                    mHandler.removeCallbacks(mRunnable);
+
+                    Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
+                    startActivity(intent);
+
+                    SplashScreenActivity.this.finish();
+                }
+            };
+
+            mHandler = new Handler();
+            mHandler.postDelayed(mRunnable, UI_DELAY);
+    }
+
+}
